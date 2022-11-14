34dc34baf6078bb271237588dac6f62e4d637a10
Max Boiko <b89048242464@yandex.ru>
2022-11-14 16:24:00 +0300

    второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 10000;
-        int bonusprice = 20;
-        int bonus = ticketprice / bonusprice;
-        System.out.println("Привет! Благодарим вас за выбор нашей авиакомпании! Ваш бонус за покупку билета составляет:"+ bonus +" милей");
+        int TicketPrice = 10000;
+        int BonusPrice = 20;
+        int bonus = TicketPrice / BonusPrice;
+        System.out.println("Привет! Благодарим вас за выбор нашей авиакомпании! Ваш бонус за покупку билета составляет:" + bonus + " милей");
     }
 }
